74c341b7904e814d252d139aad8e38bd59b99f83
ChaoWei <harry2300042@gmail.com>
2021-01-20 21:12:15 +0800

    add input process utility and apply in test

---
+++ src/utility/inputProcessUtility.hpp
@@ -0,0 +1,16 @@
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+class inputProcessUtility {
+public:
+    vector<string> buildOneDimensionalArray(string input);
+    vector<vector<string>> buildTwoDimensionalArray(string input);
+    vector<vector<int>> convertToInt(vector<vector<string>> input);
+    vector<int> convertToInt(vector<string> input);
+    string serializeToString(vector<vector<int>> input);
+    string serializeToString(vector<int> input);
+};
--- test/test.cpp
+++ test/test.cpp
@@ -1,27 +1,29 @@
 #include "../src/solution/solution.hpp"
+#include "../src/utility/inputProcessUtility.hpp"
 #include "gtest/gtest.h"
 
 
-class SolutionMultipleParametersTests : public ::testing::TestWithParam<std::tuple<vector<int>,int, vector<int>>> {
+class SolutionMultipleParametersTests : public ::testing::TestWithParam<std::tuple<string, int, string>> {
 protected:
 	solution sol;
+	inputProcessUtility utility;
 };
 
-TEST_P(SolutionMultipleParametersTests, SolutionTwoSums){
-	vector<int> inputData = std::get<0>(GetParam());
-	int inputTarget = std::get<1>(GetParam());
+TEST_P(SolutionMultipleParametersTests, solutionTwoSums){
+	string input = std::get<0>(GetParam());
+	vector<int> nums = utility.convertToInt(utility.buildOneDimensionalArray(input));
+	int n = std::get<1>(GetParam());
 
-	vector<int> expected = std::get<2>(GetParam());
-
-	ASSERT_EQ(expected, sol.twoSum(inputData, inputTarget));
+	string expected = std::get<2>(GetParam());
+	ASSERT_EQ(expected, utility.serializeToString(sol.twoSum(nums, n)));
 }
 
 INSTANTIATE_TEST_SUITE_P(
-	twoSumSolutionTest,
+	solutionTwoSumTest,
 	SolutionMultipleParametersTests,
 	::testing::Values(
-		std::make_tuple(vector<int>{2, 11, 18, 7, 15}, 13, vector<int>{0, 1}),
-		std::make_tuple(vector<int>{}, 0, vector<int>{}),
-		std::make_tuple(vector<int>{2, 11, 18, 7, 15}, 25, vector<int>{2, 3}),
-		std::make_tuple(vector<int>{2, 11, 18, 7, 15}, 0, vector<int>{})
+		std::make_tuple("[2,11,18,7,15]", 13, "[0,1]"),
+		std::make_tuple("[]", 0, "[]"),
+		std::make_tuple("[2,11,18,7,15]", 25, "[2,3]"),
+		std::make_tuple("[2,11,18,7,15]", 0, "[]")
 	));
