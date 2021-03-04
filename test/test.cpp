61fec4d6faf7081e0b1e4e7a074266cbfd5cdc9b
ChaoWei <harry2300042@gmail.com>
2021-03-05 00:49:10 +0800

    update the template
    1. add implementation
    2. add utility
    3. make the test script cleaner

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,6 @@
-#include "../solution/LFUCache.hpp"
 #include "gtest/gtest.h"
+#include "../solution/LFUCache.hpp"
+#include "../utility/utility.hpp"
 #include <vector>
 #include <string>
 #include <regex>
@@ -7,12 +8,10 @@
 using namespace std;
 
 
-class SolutionMultipleParametersTests : public ::testing::TestWithParam<std::tuple<vector<pair<string,string>>, vector<string>>> {
+class SolutionMultipleParametersTests : public ::testing::TestWithParam<std::tuple<vector<vector<string>>, vector<string>>> {
 protected:
 	LFUCache* obj;
-	vector<string> utility(vector<pair<string,string>> inputs);
-	vector<int> convertStrToArray(string s);
-	string convertArrayToStr(const vector<int>& nums);
+	vector<string> act(vector<vector<string>> inputs);
 private:
 	enum Action { init, get, put };
 	unordered_map<string, Action> dict{ 
@@ -22,55 +21,28 @@ private:
 	};
 };
 
-vector<int> SolutionMultipleParametersTests::convertStrToArray(string s){
-	std::regex e ("([.]|[^.,]+)");
-	std::regex_iterator<std::string::iterator> rit ( s.begin()+1, s.end()-1, e );
-    std::regex_iterator<std::string::iterator> rend;
-    vector<int> result;
-	
-	while (rit!=rend) {
-		result.push_back(stoi(rit->str()));
-        ++rit;
-    }
-	return result;
-}
 
-string SolutionMultipleParametersTests::convertArrayToStr(const vector<int>& nums){
-	stringstream out;
-	
-	string delimiter = "";
-	out << "[";
-	for(auto& num : nums){
-		out << delimiter << num;
-		delimiter = ",";
-	}
-	out << "]";
-
-	return out.str();
-}
-
-vector<string> SolutionMultipleParametersTests::utility(vector<pair<string,string>> inputs){
+vector<string> SolutionMultipleParametersTests::act(vector<vector<string>> inputs){
 	vector<string> result;
 	for(auto commands : inputs){
-		string input = commands.first;
-		string arg = commands.second;
+		string input = commands[0];
+		vector<string> args = buildOneDimensionalArray<string>(commands[1]);
 		Action action = dict[input];
 		int capacity, getVal, idx, putKey, putVal;
 		switch (action)
 		{
 		case Action::init:
-			capacity = stoi(arg);
+			capacity = stoi(args[0]);
 			obj = new LFUCache(capacity);
 			result.push_back("null");
 			break;
 		case Action::get:
-			getVal = stoi(arg.substr(1,arg.length()-2));
+			getVal = stoi(args[0]);
 			result.push_back(to_string(obj->get(getVal)));
 			break;
 		case Action::put:
-			idx = find(arg.begin(), arg.end(), ',')-arg.begin();
-			putKey = stoi(arg.substr(1,idx));
-			putVal = stoi(arg.substr(idx+1,arg.length()-(idx+1)-1));
+			putKey = stoi(args[0]);
+			putVal = stoi(args[1]);
 			obj->put(putKey, putVal);
 			result.push_back("null");
 			break;
@@ -82,8 +54,8 @@ vector<string> SolutionMultipleParametersTests::utility(vector<pair<string,strin
 }
 
 TEST_P(SolutionMultipleParametersTests, SolutionLFUCache){
-	vector<pair<string,string>> inputs = std::get<0>(GetParam());
-	vector<string> result = utility(inputs);
+	vector<vector<string>> inputs = std::get<0>(GetParam());
+	vector<string> result = act(inputs);
 	vector<string> expected = std::get<1>(GetParam());
 
 	ASSERT_EQ(expected, result);
@@ -94,7 +66,7 @@ INSTANTIATE_TEST_SUITE_P(
 	SolutionMultipleParametersTests,
 	::testing::Values(
 		std::make_tuple(
-			vector<pair<string,string>>
+			vector<vector<string>>
 			{
 				{ "LFUCache", "[2]" }, 
 				{ "put", "[1,1]" }, 
@@ -118,10 +90,3 @@ INSTANTIATE_TEST_SUITE_P(
 			}
 		)
 	));
-
-/*
-["LFUCache","put","put","get","put","get","get","put","get","get","get"]
-[[2],[1,1],[2,2],[1],[3,3],[2],[3],[4,4],[1],[3],[4]]
-[null,null,null,1,null,-1,3,null,-1,3,4]
-
-*/
